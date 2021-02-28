72c4cd47fc4b92203d6313803508104511881816
Sven Elflein <elflein.sven@googlemail.com>
2021-02-28 13:10:54 +0100

    Compile at installation time

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from torch.utils import cpp_extension
 
 setup(
   name='fasth',
@@ -8,6 +9,8 @@ setup(
   packages=["fasth"],
   long_description=open('README.md').read(),
   long_description_content_type="text/markdown",
+  ext_modules=[cpp_extension.CppExtension('fasth', ['fasth/fasth_cuda.cu', 'fasth/fasth.cpp'])],
+  cmdclass={'build_ext': cpp_extension.BuildExtension},
   python_requires=">=3.6",
   install_requires=[
       "torch>=1.3.1",
